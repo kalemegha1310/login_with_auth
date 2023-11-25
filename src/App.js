aede1297576d3cb54d7687faabba0dde9d962cf4
kalemegha1310 <kalemegha1310@gmsil.com>
2023-11-25 12:46:05 +0530

    auth0 code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { Profiler } from 'react';
+import Profile from './Profile';
 
 function App() {
   return (
     <div className="App">
+      <LoginButton></LoginButton>
       <p>Hello</p>
+      <Profile></Profile>
+      <LogoutButton></LogoutButton>
+  
     </div>
   );
 }
